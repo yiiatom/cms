4c017a0dacad0fb0e19a0dad2818a685f8f80f70
Dmitry Khlystov <dkhlystov@gmail.com>
2026-08-21 22:42:38 +1000

    Move current user to header

--- src/Web/Shared/Layout/Main/_sidebar.php
+++ src/Web/Shared/Layout/Main/_sidebar.php
@@ -16,37 +16,4 @@ use Yiisoft\Html\Html;
         ->addIconClass('me-2')
     ?>
     <div class="small opacity-25 px-2">v0.1.0-dev</div>
-    <hr>
-    <div class="dropup current-user px-2">
-        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            <div class="avatar">
-                <?php if ($userAvatarUrl): ?>
-                    <img src="<?= $userAvatarUrl ?>" alt="">
-                <?php else: ?>
-                    <i class="fa-regular fa-user"></i>
-                <?php endif; ?>
-            </div>
-            <strong><?= Html::encode($userDisplayName) ?></strong>
-        </a>
-        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-            <li>
-                <?= Html::a($t->translate('Profile'))
-                    ->url($urlGenerator->generate('atom.profile.edit'))
-                    ->class('dropdown-item') ?>
-            </li>
-            <li>
-                <?= Html::a($t->translate('Change Password'))
-                    ->url($urlGenerator->generate('atom.profile.change-password'))
-                    ->class('dropdown-item') ?>
-            </li>
-            <li>
-                <hr class="dropdown-divider">
-            </li>
-            <li>
-                <?= Html::a($t->translate('Log Out'))
-                    ->url($urlGenerator->generate('atom.logout'))
-                    ->class('dropdown-item') ?>
-            </li>
-        </ul>
-    </div>
 </aside>
--- src/Web/Shared/Layout/Main/main.php
+++ src/Web/Shared/Layout/Main/main.php
@@ -56,6 +56,39 @@ $this->beginPage()
         ->url($urlGenerator->generate('atom.dashboard'))
         ->class('brand text-white text-decoration-none fs-4 me-2')
     ?>
+
+    <div class="dropdown current-user ms-auto">
+        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <div class="avatar">
+                <?php if ($userAvatarUrl): ?>
+                    <img src="<?= $userAvatarUrl ?>" alt="">
+                <?php else: ?>
+                    <i class="fa-regular fa-user"></i>
+                <?php endif; ?>
+            </div>
+            <strong class="d-none d-md-inline-block ms-2"><?= Html::encode($userDisplayName) ?></strong>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+            <li>
+                <?= Html::a($t->translate('Profile'))
+                    ->url($urlGenerator->generate('atom.profile.edit'))
+                    ->class('dropdown-item') ?>
+            </li>
+            <li>
+                <?= Html::a($t->translate('Change Password'))
+                    ->url($urlGenerator->generate('atom.profile.change-password'))
+                    ->class('dropdown-item') ?>
+            </li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li>
+                <?= Html::a($t->translate('Log Out'))
+                    ->url($urlGenerator->generate('atom.logout'))
+                    ->class('dropdown-item') ?>
+            </li>
+        </ul>
+    </div>
 </header>
 
 <div class="atom-layout-wrapper">
